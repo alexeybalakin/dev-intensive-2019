2dffccad6ee85b29767d273e0d5295bc7978e9a3
alexeybalakin <kontrmail@gmail.com>
2019-07-02 13:09:44 +0300

    исправил parsing FullName

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -1,14 +1,11 @@
 package ru.skillbranch.devintensive.utils
 
 object Utils {
-    fun parseFullName(fullName:String?):Pair<String?, String?>{
-
-        val parts : List<String>? = fullName?.split(" ")
-
-        var firstName = parts?.getOrNull(0)
-        if (firstName.isNullOrBlank()) firstName = "null"
-        var lastName = parts?.getOrNull(1)
-        if (lastName.isNullOrBlank()) lastName = "null"
-//        return Pair(firstName, lastName)
-        return firstName to lastName    }
+    fun parseFullName(fullName: String?): Pair<String?, String?> {
+        val parts: List<String>? = fullName?.split(" ")?.filter { !it.isBlank() }
+        val firstName = parts?.getOrNull(0)
+        val lastName = parts?.getOrNull(1)
+        return (if (firstName.isNullOrEmpty()) null else firstName) to
+                (if (lastName.isNullOrEmpty()) null else lastName)
+    }
 }
